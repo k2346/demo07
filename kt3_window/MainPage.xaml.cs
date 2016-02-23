ccc5f935b96a5c7e618a52ac56b636f9698628d5
k2346 <k2346@student.jamk.fi>
2016-02-23 15:40:50 +0200

    kotitehtävä valmis, mutta koska matikka on paskaa niin.. ei se toimi oikein.

--- kt3_window/MainPage.xaml.cs
+++ kt3_window/MainPage.xaml.cs
@@ -26,5 +26,40 @@ namespace kt3_window
         {
             this.InitializeComponent();
         }
+
+        private void Laskebutton_Click(object sender, RoutedEventArgs e)
+        {   //etsi käyttäjän syöttämät luvut
+            // leveys
+            string leveysString = leveysTextBox.Text;
+            int leveys = int.Parse(leveysString); // muutetaan string intiksi
+            //korkeus
+            string korkeusString = korkeusTextBox.Text;
+            int korkeus = int.Parse(korkeusString); // muutetaan string intiksi, jotta laskut on mahdollisia
+            //karmi
+            string karmiString = karmitextBox.Text;
+            int karmi = int.Parse(karmiString); // muutetaan string intiksi
+
+            //lasketaan ikkunan pinta-ala
+            int ikkunaPa = (leveys * korkeus) /10;
+
+            //lasketaan piiri
+            int luku1 = (karmi * 2 + leveys);
+            int luku2 = (karmi * 2 + korkeus);
+            int piiri = ((luku1 + luku2) * 2) /10;
+
+
+        
+            //lasketaan karmin pinta-ala
+            int karmiPA = (karmi * leveys + karmi * leveys + karmi * korkeus + karmi * korkeus);
+
+            //lasketaan lasin pinta-ala
+            int lasiPa = (ikkunaPa - karmiPA) / 10;
+
+            //tulostetaan tulokset vastausbokseihin
+            VAikkunaPAtextBox.Text = ikkunaPa.ToString() + "cm^2";
+            VAkarmipiiritextBox.Text = piiri.ToString();
+            VAlasinPAtextBox.Text = lasiPa.ToString() + "cm^2";
+
+        }
     }
 }
